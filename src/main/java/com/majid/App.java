d2fe624a7bf2efaae75c22495349a17b6f68dbfe
Shaikh-Majid <ms5038248@gmail.com>
2025-12-21 22:21:51 +0530

    sprint-2

--- src/main/java/com/majid/App.java
+++ src/main/java/com/majid/App.java
@@ -3,6 +3,6 @@ package com.majid;
 public class App {
     public static void main(String[] args) {
         System.out.println("sprint-2 updated file");
-	        System.out.println("my name is ms");
+	System.out.println("my name is ms5038248");
     }
 }
