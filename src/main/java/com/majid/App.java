29fd2f523a725ac59532c2050b6a8a4f607b87c3
Shaikh-Majid <ms5038248@gmail.com>
2025-12-21 22:01:43 +0530

    sprint-2 commit update chnages

--- src/main/java/com/majid/App.java
+++ src/main/java/com/majid/App.java
@@ -3,5 +3,6 @@ package com.majid;
 public class App {
     public static void main(String[] args) {
         System.out.println("sprint-2 updated file");
+	        System.out.println("my name is ms");
     }
 }
