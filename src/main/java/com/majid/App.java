175f71c84bdeb7efcefd8f3cae5dfcea0d6eeed7
Shaikh-Majid <ms5038248@gmail.com>
2025-12-21 21:11:51 +0530

    updated the App.java file

--- src/main/java/com/majid/App.java
+++ src/main/java/com/majid/App.java
@@ -2,6 +2,6 @@ package com.majid;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("My name is majid");
     }
 }
